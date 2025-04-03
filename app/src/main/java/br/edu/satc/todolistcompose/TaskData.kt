7ce7559d8f3c005867aa36162ceca978c895732e
pauloFrancisconi <pauloffrancisconi@gmail.com>
2025-04-02 21:04:24 -0300

    adicionando a class de Data das Tasks

--- app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
+++ app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
@@ -1,7 +1,41 @@
 package br.edu.satc.todolistcompose
 
-data class TaskData (
-    val title: String,
-    val description: String,
-    val complete: Boolean
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Update
+
+@Entity
+data class TaskData(
+
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0,
+
+    @ColumnInfo(name = "task_title")
+    val taskTitle: String,
+
+    @ColumnInfo(name = "task_description")
+    val taskDescription: String,
+
+    @ColumnInfo(name = "task_complete")
+    val taskComplete: Boolean
 )
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM TaskData")
+    fun getAll(): List<TaskData>
+
+    @Update
+    fun updateAll(vararg tasks: TaskData)
+
+    @Insert
+    fun insertAll(vararg tasks: TaskData)
+
+    @Delete
+    fun delete(task: TaskData)
+}
